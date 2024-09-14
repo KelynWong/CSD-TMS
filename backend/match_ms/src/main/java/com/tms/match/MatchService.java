09535d6ffaf6b38510c5f8d3579df93f626897e8
Kai Xuan <tankaixuan24@gmail.com>
2024-09-14 15:42:28 +0800

    init match ms

---
+++ backend/match_ms/src/main/java/com/tms/match/MatchService.java
@@ -0,0 +1,13 @@
+package com.tms.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchService.class, args);
+	}
+
+}
