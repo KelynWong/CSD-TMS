24d58d53e3699d9454b229dc146d7b5d6f62e6e0
Owen Goh <owengoh2001@gmail.com>
2024-09-14 15:11:35 +0800

    updated fonts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,6 +10,13 @@ import { Button } from "@/components/ui/button";
 import { ClientPageRoot } from "next/dist/client/components/client-page";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/NavBar";
+import localFont from "next/font/local";
+
+const norwester = localFont({
+  src: "./fonts/norwester.otf",
+  variable: "--font-norwester"
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -17,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" className={norwester.variable}>
         <body>
           <header>
             <Navbar />
