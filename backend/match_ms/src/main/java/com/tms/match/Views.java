a8293661c4696c455c42fbf8505dbab5358dc288
Kai Xuan <tankaixuan24@gmail.com>
2024-09-24 16:15:26 +0800

    updated match api views

--- backend/match_ms/src/main/java/com/tms/match/Views.java
+++ backend/match_ms/src/main/java/com/tms/match/Views.java
@@ -3,4 +3,5 @@ package com.tms.match;
 public class Views {
     public static class Public {}
     public static class Internal extends Public {}
+    public static class Patch extends Public {}
 }
