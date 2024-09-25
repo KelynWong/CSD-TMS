c85dc4bf8587026bcc599ad2255fbd30f49b8f50
Owen Goh <owengoh2001@gmail.com>
2024-09-25 23:16:35 +0800

    removed the console.log statements in api

--- frontend/src/app/players/page.tsx
+++ frontend/src/app/players/page.tsx
@@ -21,7 +21,6 @@ export default function Players() {
 			setPlayerCardProps(mappedData);
 		});
 	}, []);
-	// console.log(playersCardProps);
 	return (
 		<div className="container mx-auto px-4 py-8">
 			<h1 className="text-3xl font-bold mb-6">Players</h1>
