b4bfa7214a8abaa1c4c59c085bc2e28ca6275a78
Owen Goh <owengoh2001@gmail.com>
2024-09-25 23:16:35 +0800

    removed the console.log statements in api

--- frontend/src/api/users/api.ts
+++ frontend/src/api/users/api.ts
@@ -4,7 +4,6 @@ import { Player } from "@/types/player";
 export const fetchPlayers = async (): Promise<Player[]> => {
 	try {
 		const response = await axios.get("http://localhost:8080/api/users");
-    console.log(response.data);
 		return response.data;
 	} catch (error) {
 		console.error("Error fetching players", error);
