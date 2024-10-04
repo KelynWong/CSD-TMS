49bf9f2519024166e5b26e510695f33e9de23730
Owen Goh <owengoh2001@gmail.com>
2024-10-04 18:12:53 +0800

    fix the player cards styling and added pagination

--- frontend/src/components/TournamentHistory.tsx
+++ frontend/src/components/TournamentHistory.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import TournamentCard from "@/components/TournamentCard";
 import Paginator from "@/components/Paginator";
 
