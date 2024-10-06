9907b25a6714ba7ff44f2057c20ca416c0062791
SoniaL25 <sonia.lim.2023@scis.smu.edu.sg>
2024-10-07 00:49:06 +0800

    reorganising security, exception and test files

--- backend/tournament_ms/src/main/java/com/tms/exception/PlayerNotFoundException.java
+++ backend/tournament_ms/src/main/java/com/tms/exception/PlayerNotFoundException.java
@@ -1,5 +1,4 @@
-package com.tms.tournamentplayer;
-
+package com.tms.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
---
+++ backend/tournament_ms/src/test/java/com/tms/tournament/TournamentServiceTest.java
@@ -0,0 +1,87 @@
+package com.tms.tournament;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoException.class)
+public class TournamentServiceTest {
+    
+    // @Mock
+    // private TournamentRepository tournaments;
+    // @InjectMocks
+    // private TournamentServiceImpl tournamentService;
+    // /* Unit Testing */
+    // /* addTournament() BLACKLIST
+    //  *  - tournament tile cannot be empty
+    //  *  - tournament title needs to be unique
+    //  *  - tournament startDT cannot be aft tournament endDT
+    //  *  - tournament regStartDT cannot be after tournament regEndDT
+    //  *  - regStartDT n regEndDT cannot be after tournament startDT
+    //  */
+    // @Test
+    // void addTournament_AllGood_ReturnSavedTournament() {
+    //     // Arrange
+        
+    //     // Act 
+    //     // Assert
+    // }
+    // @Test
+    // void addTournament_SameTitle_ReturnNull() {
+    //     // Arrange
+    //     Tournament tournament = new Tournament();
+        
+    //     Book book = new Book("This is a New Title");
+    //     List<Book> booksOfSameTitle = new ArrayList<Book>(); 
+    //     booksOfSameTitle.add(book);
+    //     // mock the "findbytitle" operation
+    //     when(books.findByTitle(any(String.class))).thenReturn(booksOfSameTitle);
+    //     // Act 
+    //     Book savedBook = bookService.addBook(book);
+    //     // Assert
+    //     assertNull(savedBook);
+    //     verify(books).findByTitle(book.getTitle());
+    // }
+    // @Test
+    // void addTournament_EmptyTitle_ReturnNull() {
+    // }
+    // @Test
+    // void addTournament_startDTAftEndDT_ReturnNull() {
+    // }
+    // @Test
+    // void addTournament_regStartDTAftRegEndDT_ReturnNull() {
+        
+    // }
+    // @Test
+    // void addTournament_regStartAndEndDTAftRegStartAndEndDT_ReturnNull() {
+        
+    // }
+    // /* updateTournament() BLACKLIST
+    //  *  - tournament tile cannot be empty
+    //  *  - tournament title needs to be unique
+    //  *  - tournament startDT cannot be aft tournament endDT
+    //  *  - tournament regStartDT cannot be after tournament regEndDT
+    //  *  - regStartDT n regEndDT cannot be after tournament startDT
+    //  */
+    // @Test
+    // void updateTournament_AllGood_returnSavedTournament() {
+    
+    // }
+    // @Test
+    // void updateTournament_WrongInput_ReturnNull() {
+        
+    // }
+    // @Test
+    // void updateTournament_NotFound_ReturnNull() {
+    // }
+}
