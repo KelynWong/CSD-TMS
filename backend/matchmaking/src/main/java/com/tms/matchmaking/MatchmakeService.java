799bd565b14fe935555ee360b2e8f3df19e372d0
Kai Xuan <tankaixuan24@gmail.com>
2024-10-09 17:01:56 +0800

    removed unused imports

--- backend/matchmaking/src/main/java/com/tms/matchmaking/MatchmakeService.java
+++ backend/matchmaking/src/main/java/com/tms/matchmaking/MatchmakeService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.Collections;
-import java.util.Comparator;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
