c68b585443a532530a7942e4503531c21590f1e4
SoniaL25 <sonia.lim.2023@scis.smu.edu.sg>
2024-10-09 19:11:51 +0800

    Add updateTournament Unit testing

--- backend/tournament_ms/src/test/java/com/tms/tournament/TournamentServiceTest.java
+++ backend/tournament_ms/src/test/java/com/tms/tournament/TournamentServiceTest.java
@@ -1,6 +1,7 @@
 package com.tms.tournament;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -8,6 +9,7 @@ import static org.mockito.Mockito.when;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -84,7 +86,8 @@ public class TournamentServiceTest {
         String winner = null;
         List<Player> players = new ArrayList<>();
 
-        Tournament tournament = new Tournament(id, tournamentName, startDT, endDT, status, regStartDT, regEndDT, createdBy, winner, players);        List<Tournament> tournamentOfSameName = new ArrayList<>();
+        Tournament tournament = new Tournament(id, tournamentName, startDT, endDT, status, regStartDT, regEndDT, createdBy, winner, players);        
+        List<Tournament> tournamentOfSameName = new ArrayList<>();
         tournamentOfSameName.add(tournament);
 
         // - mock methods/operations
@@ -101,9 +104,79 @@ public class TournamentServiceTest {
     }
 
     /* updateTournament() - 
-     * 1. newName
-     * 2. sameName
+     * 1. Found
+     * 2. NotFound
     */
+
+    @Test
+    void updateTournament_Found_ReturnSavedTournament() {
+        // Arrange
+        // - mock objects
+        Long id = Long.valueOf(0);
+        String tournamentName = "New Badminton Tournament";
+        LocalDateTime regStartDT = LocalDateTime.of(2024, 10, 01, 10, 00, 00);
+        LocalDateTime regEndDT = LocalDateTime.of(2024, 10, 11, 10, 00, 00);
+        LocalDateTime startDT = LocalDateTime.of(2024, 10, 21, 10, 00, 00);
+        LocalDateTime endDT = LocalDateTime.of(2024, 10, 30, 10, 00, 00);
+        String status = "Scheduled";
+        String createdBy = "admin1";
+        String winner = null;
+        List<Player> players = new ArrayList<>();
+
+        Tournament tournament = new Tournament(id, tournamentName, startDT, endDT, status, regStartDT, regEndDT, createdBy, winner, players); 
+
+        Optional<Tournament> optTournament = Optional.of(tournament);
+
+        // - mock methods/operations
+        when(tournaments.findById(id)).thenReturn(optTournament);
+        when(tournaments.save(any(Tournament.class))).thenReturn(tournament);
+
+        // Act 
+        log.info("INFO: UPDATETOURNAMENT_FOUND ACT START!");
+        Tournament savedTournament = tournamentService.updateTournament(id, tournament);
+        log.info("INFO: UPDATETOURNAMENT_FOUND ACT END!");
+
+        // Assert
+        assertNotNull(savedTournament);
+        verify(tournaments).findById(id);
+        verify(tournaments).save(tournament);
+
+    }
+
+    @Test
+    void updateTournament_NotFound_ReturnNull() {
+        // Arrange
+        // - mock objects
+        Long id = Long.valueOf(0);
+        String tournamentName = "New Badminton Tournament";
+        LocalDateTime regStartDT = LocalDateTime.of(2024, 10, 01, 10, 00, 00);
+        LocalDateTime regEndDT = LocalDateTime.of(2024, 10, 11, 10, 00, 00);
+        LocalDateTime startDT = LocalDateTime.of(2024, 10, 21, 10, 00, 00);
+        LocalDateTime endDT = LocalDateTime.of(2024, 10, 30, 10, 00, 00);
+        String status = "Scheduled";
+        String createdBy = "admin1";
+        String winner = null;
+        List<Player> players = new ArrayList<>();
+
+        Tournament tournament = new Tournament(id, tournamentName, startDT, endDT, status, regStartDT, regEndDT, createdBy, winner, players);        
+        
+        // - mock methods/operations
+        when(tournaments.findById(id)).thenReturn(Optional.empty());
+
+        // Act 
+        log.info("INFO: UPDATETOURNAMENT_NOTFOUND ACT START!");
+        Tournament savedTournament = tournamentService.updateTournament(id, tournament);
+        log.info("INFO: UPDATETOURNAMENT_NOTFOUND ACT END!");
+
+        // Assert
+        assertNull(savedTournament);
+        verify(tournaments).findById(id);
+    }
+
+
+
+
+
 
    
 
