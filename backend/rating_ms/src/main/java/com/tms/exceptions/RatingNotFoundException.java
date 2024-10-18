34afa8533c6109dcfabc8fe456c574e44c53216b
Kai Xuan <tankaixuan24@gmail.com>
2024-10-18 10:27:05 +0800

    remove rating ms

--- backend/rating_ms/src/main/java/com/tms/exceptions/RatingNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tms.exceptions;
-
-public class RatingNotFoundException extends RuntimeException {
-    public RatingNotFoundException(String userId) {
-        super("No rating exists for userId " + userId);
-    }
-}
