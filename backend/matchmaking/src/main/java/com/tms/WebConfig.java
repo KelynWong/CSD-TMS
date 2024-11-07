0ffe6596abbad6d93ccd6274d9e9f8a90b01e6ba
netteee <lynettetay48@gmail.com>
2024-11-07 12:16:54 +0800

    added  .allowCredentials(true);

--- backend/matchmaking/src/main/java/com/tms/WebConfig.java
+++ backend/matchmaking/src/main/java/com/tms/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addMapping("/**") // Adjust this to match your endpoint pattern
         .allowedOrigins("http://localhost:3000", "https://csd-tms.vercel.app") // Allow this origin
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
-        .allowedHeaders("*"); // Allow credentials (optional)
+        .allowedHeaders("*") // Allow credentials (optional)
+        .allowCredentials(true);
   }
 }
