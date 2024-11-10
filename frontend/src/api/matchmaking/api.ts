c3a29b796c00efddc23b542d0cf438fd8bcea4f3
netteee <lynettetay48@gmail.com>
2024-11-10 16:10:38 +0800

    tryna fix post matchmake

--- frontend/src/api/matchmaking/api.ts
+++ frontend/src/api/matchmaking/api.ts
@@ -28,10 +28,12 @@ export const getJwtToken = (): string | null => {
 export const matchMakeByTournamentId = async (tournament_id: number, strategy: string): Promise<boolean> => {
 	try {
 		const jwtToken = getJwtToken(); 
+		console.log(jwtToken);
+		console.log(strategy);
 		const response = await axios.post(`${URL}/${tournament_id}/strategy/${strategy}`, {
 			headers: jwtToken ? { Authorization: `Bearer ${jwtToken}` } : {},
 		});
-		return response.status === 200; 
+		return response.status === 201; 
 	} catch (error: unknown) {
 		if (axios.isAxiosError(error)) {
 			// TypeScript now knows that `error` is an AxiosError
