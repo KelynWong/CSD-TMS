8c2dabcb0832c9cc74403fd9b3020b936ce8abc7
SoniaL25 <sonia.lim.2023@scis.smu.edu.sg>
2024-11-10 15:12:13 +0800

    change isRegistered reply to player not found

--- backend/tournament_ms/src/main/java/com/tms/tournament/TournamentController.java
+++ backend/tournament_ms/src/main/java/com/tms/tournament/TournamentController.java
@@ -1,8 +1,5 @@
 package com.tms.tournament;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.util.*;
 
 import org.springframework.http.*;
