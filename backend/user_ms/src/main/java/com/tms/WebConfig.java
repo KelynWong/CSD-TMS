7127267833ed21aba98748d8643df7c9a48ba8f8
kelynwong <kelynwonget@gmail.com>
2024-11-13 20:13:12 +0800

    disable debug

--- backend/user_ms/src/main/java/com/tms/WebConfig.java
+++ backend/user_ms/src/main/java/com/tms/WebConfig.java
@@ -1,6 +1,7 @@
 package com.tms;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +10,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableWebSecurity(debug = false)
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
